0f2421ef69f65680b77700be97c949288ab29c85
Ryoga <isimaruru.1712@gmail.com>
2024-10-03 11:51:33 -0700

    Done

--- ex05.js
+++ ex05.js
@@ -1,9 +1,12 @@
 // Instructions: Use lodash's reduce function to get the product of all numbers in the array.
 
-const _ = require('lodash');
+const _ = require("lodash");
 
 const numbers = [1, 2, 3, 4];
 
 // your code here
+const product = _.reduce(numbers, (a, b) => a * b);
+// reduceの引数には
+// 累積された値（accumulator）と現在の値（currentValue）
 
 console.log(product); // Expected output: 24
--- ex08.js
+++ ex08.js
@@ -1,9 +1,16 @@
 // Instructions: Use lodash's groupBy function to group the numbers by odd and even.
 
-const _ = require('lodash');
+const _ = require("lodash");
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
 // your code here
+const groupedNumbers = _.groupBy(numbers, (num) => {
+  if (num % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+});
 
 console.log(groupedNumbers); // Expected output: { 'odd': [1, 3, 5, 7], 'even': [2, 4, 6, 8] }
